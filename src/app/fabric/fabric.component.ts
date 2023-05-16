d514b7d8beca12eefcc01fcba7302d4fefed1250
sabuja <gpattnaik1234@gmail.com>
2023-05-16 10:26:15 +0530

    Fabric compmonent

--- src/app/fabric/fabric.component.ts
+++ src/app/fabric/fabric.component.ts
@@ -1,31 +1,136 @@
 import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { fabric } from 'fabric';
-
+import { EmployeeListComponent } from '../employee-list/employee-list.component';
+import { HttpClient } from '@angular/common/http';
+import * as XLSX from 'xlsx';
+// var newData=new Array();
+// var finalString="hello";
 @Component({
   selector: 'app-fabric',
   templateUrl: './fabric.component.html',
   styleUrls: ['./fabric.component.scss'],
 })
+
 export class FabricComponent implements AfterViewInit {
+ExcelData:any;
+readExcel(event: any) {
+// throw new Error('Method not implemented.');
+let file=event.target.files[0];
+let fileReader=new FileReader();
+fileReader.readAsBinaryString(file);
+
+fileReader.onload=  (e)=>{
+  var workBook = XLSX.read(fileReader.result,{type:'binary'});
+  var sheetName = workBook.SheetNames;
+  this.ExcelData=XLSX.utils.sheet_to_json(workBook.Sheets[sheetName[0]]);
+  console.log(this.ExcelData);
+  
+  
+
+}
+}
+  CustomerArray : any[] = [];
+  isResultLoaded = false;
+  jsonString: any="";
+  // temp:String=this.jsonString[1];
+  person:any="";
+  a:any="a";
+  b:any="b";
+  c:any="c";
+  finalString="hello";
+  // Object.keys(arr)
+
+  // http: any;
+  constructor(private http: HttpClient){
+
+  }
+  
+  async getAllCustomer()
+  {
+    // console.log("error or not")
+    var arr=new Array("Harshit","Sabuja");
+    const p=this.http.get("http://localhost:8084/api/v1/customer/getAllCustomer")
+
+      .subscribe((resultData: any) => {
+        this.isResultLoaded = true;
+        // console.log(resultData);
+        this.CustomerArray = resultData;
+        console.log(typeof this.CustomerArray);
+        this.jsonString = JSON.stringify(this.CustomerArray[1]);
+        console.log(this.jsonString);
+        // console.log(typeof this.jsonString)
+        this.person = JSON.parse(this.jsonString);
+        this.a = this.person.customername;
+
+        this.b = this.person.customeraddress;
+
+        this.c = this.person.mobile;
+
+        console.log("first", this.finalString);
+        this.finalString ="  "+this.a +"\n"+"Programmer Analyst";
+        console.log("second", this.finalString);
+        
+        let abc = {"basicpay":2000,"dearness":3000};
+        let a=Object.keys(abc).forEach(element => {
+          
+          
+        });;
+        console.log(a);
+        console.log("final string", this.finalString);
+        let json = {
+          objects: [
+            {
+              type: 'text',
+              originX: 'center',
+              originY: 'center',
+              left: 50,
+              top: 50,
+              width: 80,
+              height: 50,
+              // font-size:20px,
+              text: this.finalString,
+              fontFamily: 'cursive',
+              fill: 'rgb(0, 0, 0)',
+              scaleX: 0.5,
+              scaleY: 0.4,
+              visible: true,
+              textAlign: 'left',
+              splitBy: '\n', // Split the text into lines by newline character
+            },
+          ],
+          background: '',
+        };
+        const objectList = this.canvas.getObjects();
+        console.log('objectList', objectList);
+        const backgroundImage = this.canvas.backgroundImage;
+        // console.log('canvas json', this.canvas.toJSON());
+        console.log("json data", json);
+        this.canvas.loadFromJSON(
+          json,
+          this.canvas.renderAll.bind(this.canvas),
+          function (o: any, object: any) {
+            fabric.log(o, object);
+          }
+        );
+
+        this.canvas.add.apply(this.canvas, objectList);
+        this.canvas.setBackgroundImage(
+          backgroundImage!,
+          this.canvas.renderAll.bind(this.canvas)
+        );
+      });
+    
+  }
 cleanSelect() {
   this.canvas.discardActiveObject().renderAll();
 }
   clone() {
     const activeObject = this.canvas.getActiveObject();
-    const activeGroup = this.canvas.getActiveObjects();
+    // const activeGroup = this.canvas.getActiveObjects();
 
     if (activeObject) {
       let clone;
       switch (activeObject.type) {
-        case 'rect':
-          clone = new fabric.Rect(activeObject.toObject());
-          break;
-        case 'circle':
-          clone = new fabric.Circle(activeObject.toObject());
-          break;
-        case 'triangle':
-          clone = new fabric.Triangle(activeObject.toObject());
-          break;
         case 'i-text':
           clone = new fabric.IText('', activeObject.toObject());
           break;
@@ -85,6 +190,7 @@ cleanSelect() {
     }
   }
   removeWhite(arg0: any) {
+    this.url="";
     throw new Error('Method not implemented.');
   }
 
@@ -111,10 +217,10 @@ cleanSelect() {
 
   public textString: string;
   public url: string ="";
-  public size: any = {
-    width: 500,
-    height: 800,
-  };
+  // public size: any = {
+  //   width: 500,
+  //   height: 800,
+  // };
 
   // public json: any;
   private globalEditor = false;
@@ -126,8 +232,6 @@ cleanSelect() {
   // public canvas!: fabric.Canvas;
   // public url: any = '';
   // public selected: any;
-
-  constructor() {}
 
   ngAfterViewInit(): void {
     // setup front side canvas
@@ -145,7 +249,7 @@ cleanSelect() {
       this.selected = selectedObject;
     });
     this.canvas.on('selection:updated', (e) => {
-      console.log('selected called', e.selected![0]);
+      console.log('selected updated', e.selected![0]);
       const selectedObject = e.selected![0];
       this.selected = selectedObject;
     });
@@ -240,60 +344,74 @@ cleanSelect() {
   randomId() {
     return Math.floor(Math.random() * 999999) + 1;
   }
-  json = {
-    objects: [
-      {
-        type: 'text',
-        originX: 'center',
-        originY: 'center',
-        left: 50,
-        top: 50,
-        width: 100,
-        height: 50,
-        text: 'Basic Pay: 15,000.00\nDearness Allowance: 0.00\nHouse Rent Allowance: 6000.00',
-        fontFamily: 'cursive',
-        fill: 'rgb(0, 0, 0)', // Set the text color to black
-        scaleX: 0.5,
-        scaleY: 0.5,
-        visible: true, // Set the text visibility to true
-        textAlign: 'left', // Align the text to the left
-        splitBy: '\n', // Split the text into lines by newline character
-        0: {
-          splitBy: ':',
-          styles: {
-            0: {
-              textAlign: 'left',
-            },
-            1: {
-              textAlign: 'right',
-            },
-          },
-        },
-      },
-    ],
-    background: '',
-  };
+  // json = {
+  //   objects: [
+  //     {
+  //       type: 'text',
+  //       originX: 'center',
+  //       originY: 'center',
+  //       left: 50,
+  //       top: 50,
+  //       width: 100,
+  //       height: 50,
+  //       text: this.finalString,
+  //       fontFamily: 'cursive',
+  //       fill: 'rgb(0, 0, 0)', // Set the text color to black
+  //       scaleX: 0.5,
+  //       scaleY: 0.5,
+  //       visible: true, // Set the text visibility to true
+  //       textAlign: 'left', // Align the text to the left
+  //       splitBy: '\n', // Split the text into lines by newline character
+        
+  //     },
+  //   ],
+  //   background: '',
+  // };
 
   loadJsonData() {
     console.log('loadJsonData');
+    this.getAllCustomer();
+    // console.log("final string",this.finalString);
+    // let json = {
+    //   objects: [
+    //     {
+    //       type: 'text',
+    //       originX: 'center',
+    //       originY: 'center',
+    //       left: 50,
+    //       top: 50,
+    //       width: 100,
+    //       height: 50,
+    //       text: this.finalString,
+    //       fontFamily: 'cursive',
+    //       fill: 'rgb(0, 0, 0)', // Set the text color to black
+    //       scaleX: 0.5,
+    //       scaleY: 0.5,
+    //       visible: true, // Set the text visibility to true
+    //       textAlign: 'left', // Align the text to the left
+    //       splitBy: '\n', // Split the text into lines by newline character
+          
+    //     },
+    //   ],
+    //   background: '',
+    // };
+    // const objectList = this.canvas.getObjects();
+    // console.log('objectList', objectList);
+    // const backgroundImage = this.canvas.backgroundImage;
+    // // console.log('canvas json', this.canvas.toJSON());
+    // console.log("json data",json)
+    // this.canvas.loadFromJSON(
+    //   json,
+    //   this.canvas.renderAll.bind(this.canvas),
+    //   function (o: any, object: any) {
+    //     fabric.log(o, object);
+    //   }
+    // );
 
-    const objectList = this.canvas.getObjects();
-    console.log('objectList', objectList);
-    const backgroundImage = this.canvas.backgroundImage;
-    // console.log('canvas json', this.canvas.toJSON());
-
-    this.canvas.loadFromJSON(
-      this.json,
-      this.canvas.renderAll.bind(this.canvas),
-      function (o: any, object: any) {
-        fabric.log(o, object);
-      }
-    );
-
-    this.canvas.add.apply(this.canvas, objectList);
-    this.canvas.setBackgroundImage(
-      backgroundImage!,
-      this.canvas.renderAll.bind(this.canvas)
-    );
+    // this.canvas.add.apply(this.canvas, objectList);
+    // this.canvas.setBackgroundImage(
+    //   backgroundImage!,
+    //   this.canvas.renderAll.bind(this.canvas)
+    // );
   }
 }
